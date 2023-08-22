63a784448f890d3fe5e972fd6399c46fd5bda132
Favor <favorxog@gmail.com>
2023-08-22 20:12:09 +0300

    feat: init

---
+++ nvim/after/plugin/telescope.lua
@@ -0,0 +1,63 @@
+local telescope = require("telescope")
+local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
+
+local fb_actions = telescope.extensions.file_browser.actions
+
+
+require("telescope").setup {
+    defaults = {
+        mappings = {
+            n = {
+                ["q"] = actions.close,
+            },
+        },
+        file_ignore_patterns = { "node%_modules/.*", ".git" },
+    },
+
+    extensions = {
+        file_browser = {
+            theme = "dropdown",
+            grouped = true,
+            hijack_netrw = true,
+            mappings = {
+                ["i"] = {
+                    ["<C-c>"] = function()
+                        vim.cmd("normal vbd")
+                    end,
+                },
+                ["n"] = {
+                    -- your custom normal mode mappings
+                    ["N"] = fb_actions.create,
+                    ["h"] = fb_actions.goto_parent_dir,
+                    ["/"] = function()
+                        vim.cmd("startinsert")
+                    end,
+                },
+            },
+        },
+    },
+}
+
+telescope.load_extension("file_browser")
+
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ps', function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+vim.keymap.set("n", "sf", function()
+    telescope.extensions.file_browser.file_browser({
+        path = "%:p:h",
+
+        cwd = vim.fn.expand("%:p:h"),
+        respect_gitignore = false,
+        hidden = true,
+        grouped = true,
+        previewer = false,
+        initial_mode = "normal",
+
+        layout_config = { height = 40 },
+    })
+end)
---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd [[packadd packer.nvim]]
+end
+
+require("ofavor.set")
+require("ofavor.remap")
+require("ofavor.packer")
